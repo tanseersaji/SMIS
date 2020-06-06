7eb7415f97a6a39a03dbef1132199063c815d50a
Tanseer Saji <tanseersaji@gmail.com>
2020-06-06 09:40:19 +0000

    web

--- scripts/deepfashion.sh
+++ scripts/deepfashion.sh
@@ -1,5 +1,5 @@
-python train.py --name deepfashion_smis --dataset_mode deepfashion --dataroot ~/SMIS/data/deepfashion --no_instance \
---gpu_ids 0,1,2,3 --ngf 160 --batchSize 8 --niter 60 --niter_decay 40  --model smis --netE conv --netG deepfashion
+python3 train.py --name deepfashion_smis --dataset_mode deepfashion --dataroot ~/SMIS/data/deepfashion --no_instance \
+--gpu_ids 0,1 --ngf 160 --batchSize 4 --niter 60 --niter_decay 40  --model smis --netE conv --netG deepfashion
 
-#python test.py --name deepfashion_smis --dataset_mode deepfashion --dataroot /home/zlxu/data/deepfashion --no_instance \
+#python3 test.py --name deepfashion_smis --dataset_mode deepfashion --dataroot /home/zlxu/data/deepfashion --no_instance \
 #--gpu_ids 1 --ngf 160 --batchSize 4 --model smis --netG deepfashion
